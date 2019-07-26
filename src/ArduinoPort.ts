e48b3a19bd2ea6499308ecbfd57bf001f670b11f
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-07-26 15:54:00 +0200

    auto connect back

--- src/ArduinoPort.ts
+++ src/ArduinoPort.ts
@@ -4,16 +4,24 @@ export class ArduinoPort {
     public port: SerialPort;
 
     constructor() {
-        SerialPort.list().then((ports) => {
-            this.port = new SerialPort(ports.filter((port) => port.productId == "7523")[0].comName, { baudRate: 115200 });
-        });
+        this.setPort();
     }
 
     public write(command: string) {
-        this.port.write(command, "utf8");
+        try {
+            this.port.write(command, "utf8");
+        } catch (e) {
+            this.setPort();
+        }
     }
 
     public read() {
         return this.port.read();
+    }
+
+    private setPort() {
+        SerialPort.list().then((ports) => {
+            this.port = new SerialPort(ports.filter((port) => port.productId == "7523")[0].comName, { baudRate: 115200 });
+        });
     }
 }
