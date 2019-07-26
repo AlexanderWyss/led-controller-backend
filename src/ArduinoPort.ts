aef81d962b4ca092a630093cc5c2915332c57669
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-07-26 16:04:08 +0200

    fuck the closing

--- src/ArduinoPort.ts
+++ src/ArduinoPort.ts
@@ -20,9 +20,6 @@ export class ArduinoPort {
     }
 
     private setPort() {
-        if (this.port && this.port.isOpen) {
-            this.port.close();
-        }
         SerialPort.list().then((ports) => {
             this.port = new SerialPort(ports.filter((port) => port.productId == "7523")[0].comName, { baudRate: 115200 }, (error => {this.setPort();}));
         });
