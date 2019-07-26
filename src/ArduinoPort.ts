ad27e55aebb8024b4192eed255573deb4ef04a09
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-07-26 16:06:43 +0200

    logs

--- src/ArduinoPort.ts
+++ src/ArduinoPort.ts
@@ -12,6 +12,7 @@ export class ArduinoPort {
             this.port.write(command, "utf8");
         } catch (e) {
             this.setPort();
+            console.log("error")
         }
     }
 
@@ -21,7 +22,7 @@ export class ArduinoPort {
 
     private setPort() {
         SerialPort.list().then((ports) => {
-            this.port = new SerialPort(ports.filter((port) => port.productId == "7523")[0].comName, { baudRate: 115200 }, ((error) => {this.setPort(); }));
+            this.port = new SerialPort(ports.filter((port) => port.productId == "7523")[0].comName, { baudRate: 115200 }, ((error) => {this.setPort();console.log("error2"); }));
         });
     }
 }
