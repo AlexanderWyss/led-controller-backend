4a0b60ad2714a62a49ee447d407fd2f7130daea4
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-07-26 15:40:40 +0200

    join projects

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express = require('express');
-const cors = require('cors');
+import express = require("express");
+import path = require("path");
+
+const cors = require("cors");
 const indexRouter = require("./index");
 
 const app = express();
@@ -7,4 +9,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/", indexRouter);
+app.use(express.static(path.join(__dirname, "/public/")));
 module.exports = app;
