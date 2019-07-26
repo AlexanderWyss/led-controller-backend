f7caef8e28d5ec0756b287af6e3b81a8956d6aa9
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-07-26 13:07:52 +0200

    simple website

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import { ArduinoPort } from "./ArduinoPort";
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 const port = new ArduinoPort();
 
-/* GET home page. */
+function index(res: any) {
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "../public/")
+  });
+}
+
+router.get("/", (req: any, res: any, next: any) => {
+  index(res);
+});
 router.get("/wave", (req: any, res: any, next: any) => {
   port.write("!WaveRun");
-  res.send("Wave");
+  index(res);
 });
 router.get("/rainbow", (req: any, res: any, next: any) => {
   port.write("!RainbowRun");
-  res.send("Rainbow");
+  index(res);
 });
 
 module.exports = router;
