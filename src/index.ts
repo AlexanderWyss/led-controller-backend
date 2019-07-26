137f45bc86b66e6887bdbe9936225b88c8245abe
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-07-26 13:39:42 +0200

    add commands

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
-import { ArduinoPort } from "./ArduinoPort";
-const express = require("express");
-const path = require("path");
+import {ArduinoPort} from "./ArduinoPort";
 
+const express = require("express");
 const router = express.Router();
 
 const port = new ArduinoPort();
-
-function index(res: any) {
-  res.sendFile("index.html", {
-    root: path.join(__dirname, "../public/")
-  });
-}
-
-router.get("/", (req: any, res: any, next: any) => {
-  index(res);
-});
 router.get("/wave", (req: any, res: any, next: any) => {
-  port.write("!WaveRun");
-  index(res);
+    port.write("!WaveRun");
+    res.sendStatus(200);
 });
 router.get("/rainbow", (req: any, res: any, next: any) => {
-  port.write("!RainbowRun");
-  index(res);
+    port.write("!RainbowRun");
+    res.sendStatus(200);
+});
+router.get("/rider", (req: any, res: any, next: any) => {
+    port.write("!RiderRun");
+    res.sendStatus(200);
+});
+router.get("/strobe", (req: any, res: any, next: any) => {
+    port.write("!StrobeRun");
+    res.sendStatus(200);
+});
+router.get("/running", (req: any, res: any, next: any) => {
+    port.write("!RunningRun");
+    res.sendStatus(200);
+});
+router.get("/chase", (req: any, res: any, next: any) => {
+    port.write("!ChaseRun");
+    res.sendStatus(200);
+});
+router.get("/sparkle", (req: any, res: any, next: any) => {
+    port.write("!SparkleRun");
+    res.sendStatus(200);
 });
 
 module.exports = router;
