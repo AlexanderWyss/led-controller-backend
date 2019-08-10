87271920ecbf01933fb051fbf68a1b6e6258e717
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-08-10 22:16:02 +0200

    move pattern registration to service

--- src/operation/Operation.ts
+++ src/operation/Operation.ts
@@ -17,8 +17,8 @@ export enum OperationType {
 
 export abstract class Operation {
 
-    private static PATTERN_CONTROLLERS = PatternService.getPatternControllers(Operation.ARDUINO_PORT);
     private static ARDUINO_PORT = ArduinoPort.get();
+    private static PATTERN_CONTROLLERS = PatternService.getPatternControllers(Operation.ARDUINO_PORT);
     private static GENERAL_CONTROLLER = new GeneralController(Operation.ARDUINO_PORT);
 
     protected constructor(name: string, public readonly type: OperationType) {
