3361017153f1f2c6a9c47c08b288abceb5a0c9d5
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-08-15 15:03:18 +0200

    store

--- src/DataStore.ts
+++ src/DataStore.ts
@@ -3,12 +3,19 @@ const store = new Store("LED");
 
 export class DataStore {
 
+  constructor() {
+    console.log(store.path);
+  }
+
   public set(key: string, value: any) {
+    console.log("Set to Datastore: " + key + " : " + value);
     store.set(key, value);
   }
 
 
   public get(key: string, defaultValue: any) {
-    return store.get(key, defaultValue);
+    const value = store.get(key, defaultValue);
+    console.log("Load from Datastore: " + key + " : " + value);
+    return value;
   }
 }
