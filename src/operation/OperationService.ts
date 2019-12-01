ed6c35da8a4e9a48d4a41ca6ea5514ca6ac45d14
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-12-01 19:10:29 +0100

    cron fix

--- src/operation/OperationService.ts
+++ src/operation/OperationService.ts
@@ -9,13 +9,16 @@ import {SerialportGetOperation} from "./SerialportGetOperation";
 import {SerialportSetOperation} from "./SerialportSetOperation";
 import {StartOperation} from "./StartOperation";
 import {StopOperation} from "./StopOperation";
+import {CronAddOperation} from "./CronAddOperation";
+import {CronGetOperation} from "./CronGetOperation";
+import {CronRemoveOperation} from "./CronRemoveOperation";
 
 export class OperationService {
   public static getOperations(): Operation[] {
     return [
       new AllOffOperation(), new LedsSetOperation(), new LedsGetOperation(), new OptionsOperation(),
       new PinSetOperation(), new PinGetOperation(), new SerialportGetOperation(), new SerialportSetOperation(),
-      new StartOperation(), new StopOperation()
+      new StartOperation(), new StopOperation(), new CronAddOperation(), new CronGetOperation(), new CronRemoveOperation()
     ];
   }
 }
